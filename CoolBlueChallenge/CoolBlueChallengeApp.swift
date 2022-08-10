e11a0ad0726d3fade47c40a8bc9f2c0c657526b7
Jude <jude.botchwey@monterosa.co.uk>
2022-08-10 10:59:06 +0000

    Initial Commit

---
+++ CoolBlueChallenge/CoolBlueChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBlueChallengeApp.swift
+//  CoolBlueChallenge
+//
+//  Created by Nii on 10/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolBlueChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
